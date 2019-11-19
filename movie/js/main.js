6cae110cca0ee9ab18813e37ea6867c21d8771a3
eaaakni <32707799+eaaakni@users.noreply.github.com>
2019-11-19 08:39:18 +0100

    Add files via upload

--- movie/js/main.js
+++ movie/js/main.js
@@ -10,7 +10,7 @@ let moviesJSON = {
   ]
 }
 //url til omdb api'en med key, plot og gjort klar til at tilføje title
-let url = "http://www.omdbapi.com/?apikey=1d9678d0&plot=full&t=";
+let url = "https://www.omdbapi.com/?apikey=1d9678d0&plot=full&t=";
 //Tager fat i det element fra html, som skal bruges til at indsætte dataen
 let container = document.getElementById("movieContainer");
 //et for loop laves til at køre gennem alle filmene fra moviesJSON
@@ -20,8 +20,9 @@ for (let i = 0; i < moviesJSON.movies.length; i++) {
     fetch(fetchUrl)
         .then(response => {
             return response.json();
-            //console.log(moviesJSON);
         })
+        //når data er hentet skal det bruges til at sætte siden op med
+        //skaber elementerne med createElement, tilføjer derefter attributes og tekst
         .then(data => {
             const box = document.createElement("section");
             box.setAttribute("class", "slide");
@@ -35,6 +36,7 @@ for (let i = 0; i < moviesJSON.movies.length; i++) {
             h3.setAttribute("class", "movieTitle");
             h3.innerText = data.Title;
 
+            //UDKOMMENTERET - prøver at lave en modal box men har ikke fået det løst
             // const button = document.createElement("button");
             // button.setAttribute("class", "modal-button");
             // button.setAttribute("href", "#myModal" + moviesJSON.movies[i].id);
@@ -46,28 +48,30 @@ for (let i = 0; i < moviesJSON.movies.length; i++) {
             modal.setAttribute("id", "myModal" + moviesJSON.movies[i].id);
             //modal.setAttribute("id", "myModal");
 
+            //for at finde ud af hvor gammel filmene er hentes det nuværende og udgivningsåret bliver trukket fra
             var now = new Date();
             var year = now.getFullYear();
             var dateDiff = year - data.Year;
-            console.log(dateDiff);
 
             const modalContent = document.createElement("div");
             modalContent.setAttribute("class", "modal-content");
 
+            //til at hente få video der passer til filmen, har jeg linket til embed youtube, for enden af dette tilføjes videoID fra JSON derved skabes der et url der mathcer.
             const video = document.createElement("iframe");
             video.setAttribute("src", "https://www.youtube.com/embed/"+moviesJSON.movies[i].videoID)
             video.setAttribute("class", "trailer");
 
+            //I denne del har jeg skabt hele html'et der skal komme med informationer om filmene, istedet for at tilføje elementerne en efter en.
             const information = document.createElement("div");
             information.setAttribute("class", "information");
             information.innerHTML = "<p><b>Release Year:</b> " + data.Year + " (" + dateDiff + " years old)</p><p><b>Plot:</b> "+ data.Plot +"</p><p><b>Actors:</b> " + data.Actors + "</p><p><b>Runtime:</b> " + data.Runtime + "</p><p><b>Awards:</b> " + data.Awards + "</p>";
 
-
+            //UDKOMMENTERET - mere til modal (lukke knap)
             //const close = document.createElement("span");
             // close.setAttribute("class", "close");
             // close.innerText = "x";
 
-
+            //Her sættes alle de elementer der er blevet skabt ind i HTML,
             box.appendChild(posterContainer);
             posterContainer.appendChild(poster);
             box.appendChild(h3);
@@ -77,15 +81,15 @@ for (let i = 0; i < moviesJSON.movies.length; i++) {
             modal.appendChild(modalContent)
             modalContent.appendChild(video)
             modalContent.appendChild(information)
+            //den sidste er den der binder det fast med det element fra HTML som der blev taget fat i tidligere
             container.appendChild(box);
 
         })
 }
 
-
+//UDKOMMENTERET - Det sidste forsøg jeg lavede med at sætte modalboksen op
 // console.log(document.querySelectorAll("button"));
 //
-// function
 // //https://stackoverflow.com/questions/40645032/creating-multiple-modals-on-a-single-page
 // // Get the button that opens the modal
 // let btn = document.querySelectorAll("button.modal-button");
